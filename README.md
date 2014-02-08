a88b3184c31708c94708e9e48ce7bd8cf1247822
Johnny Bell <hi@j1creative.com.au>
2014-02-08 23:12:52 +1100

    added the files, upated readme

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-Page-Scroll
+Page Scroll
 ===========
 
-A mobile friendly page scroll
+Hello Friend,
+
+After spending a couple of weeks trying to get a jQuery easy page scrolling script that would work on iPads, and iPhones I decided to combine a few of them to create this one.
+
+Please use http://gsgd.co.uk/sandbox/jquery/easing/ for awesome easing!
+
+Thanks
+
+Just updated it with an example. 
+
+Enjoy
---
+++ page-scroll.js
@@ -0,0 +1,67 @@
+ // Page Scroll Script
+
+ 
+ $(document).ready(function()
+
+   {         
+
+      $('a[href*=#]').click(function() {
+
+         // skip Page Scroll on your sliders etc. 
+
+         if($(this).parent().hasClass('scrollable_navigation') || $(this).attr('href').indexOf('javascript')>-1) return;
+
+         // duration in ms
+
+         var duration=800;
+         
+         // easing values: Check the easing script - http://gsgd.co.uk/sandbox/jquery/easing/ or use swing / linear
+
+         var easing='linear';
+
+         // Set your parameters
+
+         var newHash=this.hash;
+
+         var oldLocation=window.location.href.replace(window.location.hash, '');
+
+         var newLocation=this;
+
+         // make sure it's the same location      
+
+         if(oldLocation+newHash==newLocation)
+
+         {
+            // get target
+
+            var target=$(this.hash+', a[name='+this.hash.slice(1)+']').offset().top;
+
+            // adjust target for anchors near the bottom of the page
+
+            if(target > $(document).height()-$(window).height()) target=$(document).height()-$(window).height();         
+
+            // set selector
+
+            if($.browser.safari) var animationSelector='body:not(:animated)';
+
+            else var animationSelector='html:not(:animated)';
+
+            // animate to target and set the hash to the window.location after the animation
+
+            $(animationSelector).animate({ scrollTop: target }, duration, easing, function() {
+
+               // add new hash to the browser location
+
+               window.location.href=newLocation;
+
+            });
+
+            // cancel default click action
+
+            return false;
+
+         }
+
+      });
+
+   });
